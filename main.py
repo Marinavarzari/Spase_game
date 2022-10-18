8ca8a3168c0d102f9224f78041c9651e4f2f6567
Marinavarzari <112946469+Marinavarzari@users.noreply.github.com>
2022-10-19 00:23:07 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import pygame, controls
+from gun import Gun
+from pygame.sprite import Group
+
+
+def run ():
+    pygame.init()
+    screen = pygame.display.set_mode((700,600))
+    pygame.display.set_caption("Space defenders")
+    bg_color = (0,0,0)
+    gun = Gun(screen)
+    bullets = Group()
+    inos = Group()
+    controls.create_army(screen, inos)
+    
+    while True:
+        controls.events(screen, gun, bullets)
+        gun.update_gun()
+        controls.update(bg_color, screen, gun, inos, bullets)
+        controls.update_bullets(bullets)
+        controls.update_inos(inos)
+run()
